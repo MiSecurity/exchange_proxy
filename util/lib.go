43d4d29e7a290efa578f40bf5f5cec4432baa74d
hartnett <x@xsec.io>
2019-03-12 13:28:08 +0800

    [#] modify Copyright

--- util/lib.go
+++ util/lib.go
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2018 sec.lu
+Copyright (c) 2018 sec.xiaomi.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
